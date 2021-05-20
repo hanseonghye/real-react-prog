07fb9c5e617944481b34ea7daed728ac8f2c4552
hanseonghye <hans12312@naver.com>
2021-05-21 00:25:41 +0900

    프로젝트 설정하기

--- whois/src/search/container/Search.js
+++ whois/src/search/container/Search.js
@@ -1,6 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from'react';
+import {Col, Row, Typography} from "antd";
+import Settings from "../component/Settings";
 
 export default function Search() {
-  return <div></div>
+  return (
+      <>
+        <Row justify="end" style={{padding: 20}}>
+          <Col><Settings logout={() => {}}/></Col>
+        </Row>
+        <Row justify="center" style={{marginTop: 100}}>
+            <Col><Typography.Title style={{fontFamily: '궁서체'}}>찾 아 야 한 다</Typography.Title></Col>
+        </Row>
+        <Row justify="center" style={{marginTop: 50}}>
+          <Col span={12}>검색</Col>
+        </Row>
+      </>
+  );
 }
